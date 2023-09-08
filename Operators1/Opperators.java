359dabd12f101f3e363b28ecdd87d6cd065f88ac
EnriquedeCastro <143217608+EnriquedeCastro@users.noreply.github.com>
2023-09-08 11:34:14 -0700

    Add files via upload

---
+++ Operators1/Opperators.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Opperators 
+{
+    public static void main(String[] args) {
+        int i = 10;
+        double f = 2.5;
+        f += i;
+        boolean flag = true;
+        flag = !flag;
+        i += 1;
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(flag);
+    }
+}
